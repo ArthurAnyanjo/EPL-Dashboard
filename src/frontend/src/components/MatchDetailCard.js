566b71c7f51fb39f9e384e3d59c0d6485b453df6
arthur <aa19341@essex.ac.uk>
2022-06-09 13:24:27 +0100

    Completed UI elements for TeamPage

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,12 +1,19 @@
 import { React } from 'react'
+import { Link } from 'react-router-dom';
 
-export const MatchDetailCard = ({match}) => {
+export const MatchDetailCard = ({ teamName, match }) => {
   if (!match) return null;
+  const otherTeam = match.homeTeam === teamName ? match.awayTeam : match.homeTeam;
+  const otherTeamRoute = `/teams/${otherTeam}`;
   return (
     <div className="MatchDetailCard">
-     <h3>Latest Matches</h3>
-     <h4>Match Details</h4>
-     <h4>{match.homeTeam} vs {match.awayTeam}</h4>
+      <h3>Latest Matches</h3>
+      <h1>vs <Link to={otherTeamRoute}>{otherTeam}</Link></h1>
+      <h2>{match.date}</h2>
+      <h2>Matchweek: {match.mw}</h2>
+      <h4>Home Team Form: {match.htformPtsStr}</h4>
+      <h4>Away Team Form: {match.atformPtsStr}</h4>
+      <h3>Full Time Score: {match.fthg} : {match.ftag}</h3>
     </div>
   );
 }
--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,9 +1,14 @@
 import { React } from 'react'
+import { Link } from 'react-router-dom';
 
-export const MatchSmallCard = ({match}) => {
+export const MatchSmallCard = ({ match, teamName }) => {
+  if (!match) return null;
+  const otherTeam = match.homeTeam === teamName ? match.awayTeam : match.homeTeam;
+  const otherTeamRoute = `/teams/${otherTeam}`;
   return (
     <div className="MatchSmallCard">
-        <p>{match.homeTeam} vs {match.awayTeam}</p>
+      <h3>vs <Link to={otherTeamRoute}>{otherTeam}</Link> </h3>
+      <p>Score: {match.fthg} : {match.ftag}</p>
     </div>
   );
 }
